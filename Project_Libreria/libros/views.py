afe8c2a9a8e0f48bdc084fe3da1e992ce5d59418
pablo <pablotebb@hotmail.com>
2025-10-14 18:25:47 +0100

    feat(app):Crear los formularios usando forms.py, conectarlo a una vista y aplicar validaciones del lado del servidor (parte del trabajo)

--- Project_Libreria/libros/views.py
+++ Project_Libreria/libros/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .forms import Formulario_libros
 
 # Create your views here.
 def home(request):
+    formulario_libros = Formulario_libros()
     return render(request, 'libros/home.html')
